da8afb2dabda764b99d37452af45ff86988fc302
UditNayak <uditnayak027@gmail.com>
2024-06-22 23:37:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState("");
+  const [count, setCount] = useState(0);
+  const incrementCount = () => setCount(count + 1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type='text'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={incrementCount}>Increment Counter</button>
+      
     </div>
   );
 }
